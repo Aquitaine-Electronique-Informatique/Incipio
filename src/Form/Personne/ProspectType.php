59935c5f1aa5b47508420b554aba2abf2577e14e
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2019-09-27 16:15:12 +0200

    prospect: fix namespace for type

--- src/Form/Personne/ProspectType.php
+++ src/Form/Personne/ProspectType.php
@@ -18,6 +18,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class ProspectType extends AbstractType
 {
