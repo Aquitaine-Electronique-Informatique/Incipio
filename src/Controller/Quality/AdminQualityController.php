f07f7bbee77cd914820548678f1de48726265833
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2019-10-10 16:44:22 +0200

    navbar: AEI links

--- src/Controller/Quality/AdminQualityController.php
+++ src/Controller/Quality/AdminQualityController.php
@@ -27,25 +27,46 @@ class AdminQualityController extends Controller
     public function reactivityIndex()
     {
         $em = $this->getDoctrine()->getManager();
-        $current_question = $em->getRepository(ReactivityQuestion::class)->findOneBy([], ['id' => 'desc']);
-        $questions = $this->processReactivityStats($em->getRepository(ReactivityQuestion::class)->findAll([], ['date' => 'asc']));
+        $current_question = $em
+            ->getRepository(ReactivityQuestion::class)
+            ->findOneBy([], ['id' => 'desc']);
+        $questions = $this->processReactivityStats(
+            $em
+                ->getRepository(ReactivityQuestion::class)
+                ->findAll([], ['date' => 'asc'])
+        );
         $pole_names = $this->processPoleNames();
         $isAdmin = true;
         $canGenerate = false;
         $questionOngoing = false;
         $answers = [];
         if ($current_question != null) {
-            $answers = $em->getRepository(ReactivityAnswer::class)->getReactivityAnswerListFromQuestion($current_question->getId());
+            $answers = $em
+                ->getRepository(ReactivityAnswer::class)
+                ->getReactivityAnswerListFromQuestion(
+                    $current_question->getId()
+                );
             if ($current_question->isFinished()) {
-                if (!$em->getRepository(ReactivityQuestion::class)->hasPoleStats($current_question->getId()))
+                if (
+                    !$em
+                        ->getRepository(ReactivityQuestion::class)
+                        ->hasPoleStats($current_question->getId())
+                ) {
                     $canGenerate = true;
-            }
-            else {
+                }
+            } else {
                 $questionOngoing = true;
             }
         }
-        return $this->render('Quality/Default/reactivity_index.html.twig', 
-        ['current_question' => $current_question, 'answers' => $answers, 'questions' => $questions, 'isAdmin' => $isAdmin, 'canGenerate' => $canGenerate, 'displayQuestionForm' => !$questionOngoing, 'pole_names' => $pole_names]);
+        return $this->render('Quality/Default/reactivity_index.html.twig', [
+            'current_question' => $current_question,
+            'answers' => $answers,
+            'questions' => $questions,
+            'isAdmin' => $isAdmin,
+            'canGenerate' => $canGenerate,
+            'displayQuestionForm' => !$questionOngoing,
+            'pole_names' => $pole_names
+        ]);
     }
 
     /**
@@ -55,37 +76,57 @@ class AdminQualityController extends Controller
     public function moodIndex(MoodReasons $mood_service)
     {
         $em = $this->getDoctrine()->getManager();
-        $current_question = $em->getRepository(MoodQuestion::class)->findOneBy([], ['id' => 'desc']);
-        $questions = $this->processMoodStats($em->getRepository(MoodQuestion::class)->findAll([], ['date' => 'asc']));
+        $current_question = $em
+            ->getRepository(MoodQuestion::class)
+            ->findOneBy([], ['id' => 'desc']);
+        $questions = $this->processMoodStats(
+            $em
+                ->getRepository(MoodQuestion::class)
+                ->findAll([], ['date' => 'asc'])
+        );
         $pole_names = $this->processPoleNames();
         $mood_reasons = $mood_service->getReasons();
         $isAdmin = true;
         $canGenerate = false;
         $questionOngoing = false;
         if ($current_question != null) {
-            $answers = $em->getRepository(MoodAnswer::class)->getMoodAnswerListFromQuestion($current_question->getId());
+            $answers = $em
+                ->getRepository(MoodAnswer::class)
+                ->getMoodAnswerListFromQuestion($current_question->getId());
             if ($current_question->isFinished()) {
-                if (!$current_question->getHasMoodStats())
+                if (!$current_question->getHasMoodStats()) {
                     $canGenerate = true;
-            }
-            else {
+                }
+            } else {
                 $questionOngoing = true;
             }
+        } else {
+            $answers = null;
         }
-        else {
-            $answers = NULL;
-        }
-        return $this->render('Quality/Default/mood_index.html.twig', 
-        ['current_question' => $current_question, 'answers' => $answers, 'questions' => $questions, 'isAdmin' => $isAdmin, 'canGenerate' => $canGenerate, 'displayQuestionForm' => !$questionOngoing, 'pole_names' => $pole_names, 'mood_reasons' => $mood_reasons]);
+        return $this->render('Quality/Default/mood_index.html.twig', [
+            'current_question' => $current_question,
+            'answers' => $answers,
+            'questions' => $questions,
+            'isAdmin' => $isAdmin,
+            'canGenerate' => $canGenerate,
+            'displayQuestionForm' => !$questionOngoing,
+            'pole_names' => $pole_names,
+            'mood_reasons' => $mood_reasons
+        ]);
     }
 
-    public function processPoleNames() 
+    public function processPoleNames()
     {
         $em = $this->getDoctrine()->getManager();
-        $pole_names = $em->createQueryBuilder()->select('p.nom')->from(Pole::class, 'p')->orderBy('p.nom', 'ASC')
-        ->getQuery()->getResult();
+        $pole_names = $em
+            ->createQueryBuilder()
+            ->select('p.nom')
+            ->from(Pole::class, 'p')
+            ->orderBy('p.nom', 'ASC')
+            ->getQuery()
+            ->getResult();
         foreach ($pole_names as $key => $pole_name) {
-            $pole_names[$key] = "'".$pole_name['nom']."'";
+            $pole_names[$key] = "'" . $pole_name['nom'] . "'";
         }
         return $pole_names;
     }
@@ -102,7 +143,7 @@ class AdminQualityController extends Controller
             }
             $question->results = $results;
         }
-        return $questions; 
+        return $questions;
     }
 
     public function processMoodStats($questions)
@@ -119,7 +160,7 @@ class AdminQualityController extends Controller
             $question->results = $results;
             $question->attendency = $attendency;
         }
-        return $questions; 
+        return $questions;
     }
 
     /**
@@ -128,11 +169,15 @@ class AdminQualityController extends Controller
      */
     public function newReactivityQuestion(Request $request)
     {
-        $success_message = 'Votre question pour le test de réactivité a été envoyée';
+        $success_message =
+            'Votre question pour le test de réactivité a été envoyée';
         $em = $this->getDoctrine()->getManager();
         $reactivity_question = new ReactivityQuestion();
 
-        $form = $this->createForm(ReactivityQuestionType::class, $reactivity_question);
+        $form = $this->createForm(
+            ReactivityQuestionType::class,
+            $reactivity_question
+        );
 
         if ('POST' == $request->getMethod()) {
             $form->handleRequest($request);
@@ -140,30 +185,35 @@ class AdminQualityController extends Controller
                 $em->persist($reactivity_question);
                 $em->flush();
 
-                $members = $em->getRepository(Membre::class)->getMembresAdminCourant();
+                $members = $em
+                    ->getRepository(Membre::class)
+                    ->getMembresAdminCourant();
                 $bcc = [];
                 $members_count = count($members);
                 $i = 0;
                 foreach ($members as $key => $member) {
-		            if ($i == 0)
+                    if ($i == 0) {
                         $to = $member->getEmailEMSE();
-                    else 
+                    } else {
                         $bcc[] = $member->getEmailEMSE();
-                    $i++;    
+                    }
+                    $i++;
                 }
 
-                $message = (new \Swift_Message('[Test de Réactivité] Nouveau test'))
+                $message = (new \Swift_Message(
+                    '[Test de Réactivité] Nouveau test'
+                ))
                     ->setFrom('robot@junior-aei.com')
                     ->setTo($to)
                     ->setBcc($bcc)
                     ->setBody(
                         $this->renderView(
-                        'Quality/Emails/reactivity_question_mail.html.twig',
-                        array('question' => $reactivity_question->getText())
-                    ),
-                    'text/html'
-                );
-                
+                            'Quality/Emails/reactivity_question_mail.html.twig',
+                            array('question' => $reactivity_question->getText())
+                        ),
+                        'text/html'
+                    );
+
                 $this->get('mailer')->send($message);
 
                 $this->addFlash('success', $success_message);
@@ -171,17 +221,18 @@ class AdminQualityController extends Controller
         }
 
         return $this->render('Quality/Default/new_question.html.twig', [
-                    'form' => $form->createView(),
-                ]);
+            'form' => $form->createView()
+        ]);
     }
 
     /**
      * @Security("has_role('ROLE_QUALITE')")
      * @Route("/mood/new_question", name="quality_new_mood_question", methods={"GET", "POST"})
      */
-    public function newMoodQuestion() 
+    public function newMoodQuestion()
     {
-        $success_message = 'Votre question pour le test de réactivité a été envoyée';
+        $success_message =
+            'Votre question pour le test de réactivité a été envoyée';
         $em = $this->getDoctrine()->getManager();
         $mood_question = new MoodQuestion();
         $em->persist($mood_question);
@@ -192,11 +243,12 @@ class AdminQualityController extends Controller
         $members_count = count($members);
         $i = 0;
         foreach ($members as $key => $member) {
-            if ($i == 0)
+            if ($i == 0) {
                 $to = $member->getEmailEMSE();
-            else 
-                $bcc[] = $member->getEmailEMSE();   
-            $i++; 
+            } else {
+                $bcc[] = $member->getEmailEMSE();
+            }
+            $i++;
         }
 
         $message = (new \Swift_Message('[Test de Moral] Nouveau test'))
@@ -204,16 +256,15 @@ class AdminQualityController extends Controller
             ->setTo($to)
             ->setBody(
                 $this->renderView(
-                'Quality/Emails/mood_question_mail.html.twig'
-            ),
-            'text/html'
-        );
-        
-        $this->get('mailer')->send($message);
-        
-        $this->addFlash('success', $success_message);
-        return $this->redirectToRoute('quality_admin_mood', []);
+                    'Quality/Emails/mood_question_mail.html.twig'
+                ),
+                'text/html'
+            );
 
+        $this->get('mailer')->send($message);
+
+        $this->addFlash('success', $success_message);
+        return $this->redirectToRoute('quality_mood_admin', []);
     }
 
     /**
@@ -222,41 +273,62 @@ class AdminQualityController extends Controller
      */
     public function generateReactivityStats($id)
     {
-        $success_message = 'Les statistiques de réactivité de ce mois ont bien été générées';
-        $warning_message = 'Les statistiques de réactivité de ce mois ont déjà été générées';
+        $success_message =
+            'Les statistiques de réactivité de ce mois ont bien été générées';
+        $warning_message =
+            'Les statistiques de réactivité de ce mois ont déjà été générées';
 
         $em = $this->getDoctrine()->getManager();
         $question = $em->getRepository(ReactivityQuestion::class)->find($id);
-        $question_expiration = $question->getAskedAt()->add(DateInterval::createfromdatestring('+1 day'));
+        $question_expiration = $question
+            ->getAskedAt()
+            ->add(DateInterval::createfromdatestring('+1 day'));
         $current_prom = $em->getRepository(Membre::class)->getLastPromo();
 
         if (!$question->getHasPolestats()) {
             $poles = $em->getRepository(Pole::class)->findAll();
             foreach ($poles as $pole) {
-                $answers = $em->getRepository(ReactivityAnswer::class)->getReactivityAnswerListFromQuestionPole($question->getId(), $pole->getId());
-                $members_nb = count($em->getRepository(Membre::class)->getMembresPoleCourant($pole->getId()));
+                $answers = $em
+                    ->getRepository(ReactivityAnswer::class)
+                    ->getReactivityAnswerListFromQuestionPole(
+                        $question->getId(),
+                        $pole->getId()
+                    );
+                $members_nb = count(
+                    $em
+                        ->getRepository(Membre::class)
+                        ->getMembresPoleCourant($pole->getId())
+                );
                 $answers_count = count($answers);
-                $no_answer = 1.0 * ($members_nb - $answers_count)/$members_nb;
-                $bad_answer = 0.;
-                $good_answer = 0.;
-                foreach($answers as $answer) {
+                $no_answer =
+                    (1.0 * ($members_nb - $answers_count)) / $members_nb;
+                $bad_answer = 0;
+                $good_answer = 0;
+                foreach ($answers as $answer) {
                     if ($answer->getAnsweredAt() > $question_expiration) {
-                        $no_answer += 1/$members_nb;
+                        $no_answer += 1 / $members_nb;
                     } else {
                         if ($answer->getIsValidated()) {
-                            $good_answer += 1/$members_nb;
+                            $good_answer += 1 / $members_nb;
                         } else {
-                            $bad_answer += 1/$members_nb;
+                            $bad_answer += 1 / $members_nb;
                         }
                     }
                 }
                 $month = intval($question->getAskedAt()->format('m'));
 
-                $this->createPoleStat($current_prom, $month, $question, $pole, $no_answer * 100, $bad_answer * 100, $good_answer * 100);
+                $this->createPoleStat(
+                    $current_prom,
+                    $month,
+                    $question,
+                    $pole,
+                    $no_answer * 100,
+                    $bad_answer * 100,
+                    $good_answer * 100
+                );
             }
-            $this->addFlash('success', $success_message);        
-        }
-        else {
+            $this->addFlash('success', $success_message);
+        } else {
             $this->addFlash('warning', $warning_message);
         }
         return $this->redirectToRoute('quality_admin_reactivity', []);
@@ -268,41 +340,69 @@ class AdminQualityController extends Controller
      */
     public function generateMoodStats($id)
     {
-        $success_message = 'Les statistiques de moral de ce mois ont bien été générées';
-        $warning_message = 'Les statistiques de moral de ce mois ont déjà été générées';
+        $success_message =
+            'Les statistiques de moral de ce mois ont bien été générées';
+        $warning_message =
+            'Les statistiques de moral de ce mois ont déjà été générées';
 
         $em = $this->getDoctrine()->getManager();
         $question = $em->getRepository(MoodQuestion::class)->find($id);
-        $question_expiration = $question->getAskedAt()->add(DateInterval::createfromdatestring('+1 day'));
+        $question_expiration = $question
+            ->getAskedAt()
+            ->add(DateInterval::createfromdatestring('+1 day'));
         $current_prom = $em->getRepository(Membre::class)->getLastPromo();
 
         if (!$question->getHasMoodStats()) {
             $poles = $em->getRepository(Pole::class)->findAll();
             foreach ($poles as $pole) {
-                $answers = $em->getRepository(MoodAnswer::class)->getMoodAnswerListFromQuestionPole($question->getId(), $pole->getId());
-                $members_nb = count($em->getRepository(Membre::class)->getMembresPoleCourant($pole->getId()));
+                $answers = $em
+                    ->getRepository(MoodAnswer::class)
+                    ->getMoodAnswerListFromQuestionPole(
+                        $question->getId(),
+                        $pole->getId()
+                    );
+                $members_nb = count(
+                    $em
+                        ->getRepository(Membre::class)
+                        ->getMembresPoleCourant($pole->getId())
+                );
                 $answers_count = count($answers);
-                $no_answer = 1.0 * ($members_nb - $answers_count)/$members_nb;
-                $good_answer = 1.0 * $answers_count/$members_nb;
+                $no_answer =
+                    (1.0 * ($members_nb - $answers_count)) / $members_nb;
+                $good_answer = (1.0 * $answers_count) / $members_nb;
                 $medium_mood = 0;
-                foreach($answers as $answer) {
+                foreach ($answers as $answer) {
                     $medium_mood += $answer->getAnswer1();
                 }
-                $medium_mood = 1.0 * $medium_mood/$answers_count;
+                $medium_mood = (1.0 * $medium_mood) / $answers_count;
                 $month = intval($question->getAskedAt()->format('m'));
 
-                $this->createMoodStat($current_prom, $month, $question, $pole, $no_answer * 100, $medium_mood * 10, $good_answer * 100);
+                $this->createMoodStat(
+                    $current_prom,
+                    $month,
+                    $question,
+                    $pole,
+                    $no_answer * 100,
+                    $medium_mood * 10,
+                    $good_answer * 100
+                );
             }
-            $this->addFlash('success', $success_message);        
-        }
-        else {
+            $this->addFlash('success', $success_message);
+        } else {
             $this->addFlash('warning', $warning_message);
         }
         return $this->redirectToRoute('quality_admin_mood', []);
     }
 
-    public function createPoleStat($current_prom, $month, $question, $pole, $no_answer, $bad_answer, $good_answer) 
-    {
+    public function createPoleStat(
+        $current_prom,
+        $month,
+        $question,
+        $pole,
+        $no_answer,
+        $bad_answer,
+        $good_answer
+    ) {
         $em = $this->getDoctrine()->getManager();
         $polestat = new PoleStat();
 
@@ -316,11 +416,17 @@ class AdminQualityController extends Controller
 
         $em->persist($polestat);
         $em->flush();
-
     }
 
-    public function createMoodStat($current_prom, $month, $question, $pole, $no_answer, $medium_mood, $answer) 
-    {
+    public function createMoodStat(
+        $current_prom,
+        $month,
+        $question,
+        $pole,
+        $no_answer,
+        $medium_mood,
+        $answer
+    ) {
         $em = $this->getDoctrine()->getManager();
         $polestat = new MoodStat();
 
@@ -334,9 +440,5 @@ class AdminQualityController extends Controller
 
         $em->persist($polestat);
         $em->flush();
-
     }
-
-
-
 }
