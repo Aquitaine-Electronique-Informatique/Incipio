e1c4e1b6ababd5a725549356a3a40cd378a735c6
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2019-10-13 16:15:09 +0200

    serviceWorker: deactivated

--- public/js/pwa.js
+++ public/js/pwa.js
@@ -1,12 +1,13 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    });
-  }
-  
+// Deactivated until no problem with it
+
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', function() {
+//       navigator.serviceWorker.register('/sw.js').then(function(registration) {
+//         // Registration was successful
+//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//       }, function(err) {
+//         // registration failed :(
+//         console.log('ServiceWorker registration failed: ', err);
+//       });
+//     });
+//   }
