bf787f7cf11d3af8368fbe99f5ba565e8a2e6c52
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2019-12-13 17:01:36 +0100

    document: download from membres and "voir" view

--- src/Form/Personne/MembreType.php
+++ src/Form/Personne/MembreType.php
@@ -33,55 +33,84 @@ class MembreType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('personne', PersonneType::class, ['label' => ' ', 'user' => true])
-            ->add('identifiant', TextType::class,
-                ['label' => 'Identifiant',
-                 'required' => false,
-                 'attr' => ['readonly' => true],
-                ]
-            )
-            ->add('emailEMSE', TextType::class, ['label' => 'Email Ecole', 'required' => false])
-            ->add('promotion', IntegerType::class, ['label' => 'Promotion', 'required' => false])
-            ->add('dateDeNaissance', GenemuDateType::class,
-                ['label' => 'Date de naissance (jj/mm/aaaa)', 'widget' => 'single_text', 'format' => 'dd/MM/yyyy',
-                 'required' => false,
-                ])
-            ->add('lieuDeNaissance', TextType::class, ['label' => 'Lieu de naissance', 'required' => false])
-            ->add('nationalite', Select2CountryType::class,
-                ['label' => 'Nationalité', 'required' => true, 'preferred_choices' => ['FR']])
+            ->add('personne', PersonneType::class, [
+                'label' => ' ',
+                'user' => true
+            ])
+            ->add('identifiant', TextType::class, [
+                'label' => 'Identifiant',
+                'required' => false,
+                'attr' => ['readonly' => true]
+            ])
+            ->add('emailEMSE', TextType::class, [
+                'label' => 'Email Ecole',
+                'required' => false
+            ])
+            ->add('promotion', IntegerType::class, [
+                'label' => 'Promotion',
+                'required' => false
+            ])
+            ->add('dateDeNaissance', GenemuDateType::class, [
+                'label' => 'Date de naissance (jj/mm/aaaa)',
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+                'required' => false
+            ])
+            ->add('lieuDeNaissance', TextType::class, [
+                'label' => 'Lieu de naissance',
+                'required' => false
+            ])
+            ->add('nationalite', Select2CountryType::class, [
+                'label' => 'Nationalité',
+                'required' => true,
+                'preferred_choices' => ['FR']
+            ])
             ->add('mandats', CollectionType::class, [
                 'entry_type' => MandatType::class,
                 'allow_add' => true,
                 'allow_delete' => true,
                 'prototype' => true,
-                'by_reference' => false, //indispensable cf doc
+                'by_reference' => false //indispensable cf doc
             ])
             ->add('competences', Select2EntityType::class, [
                 'class' => Competence::class,
                 'by_reference' => false,
                 'multiple' => true,
-                'required' => false,
+                'required' => false
             ])
-            ->add('dateConventionEleve', GenemuDateType::class,
-                ['label' => 'Date de Signature de la Convention Elève', 'format' => 'dd/MM/yyyy', 'required' => false,
-                 'widget' => 'single_text',
-                ])
+            ->add('dateConventionEleve', GenemuDateType::class, [
+                'label' => 'Date de Signature de la Convention Elève',
+                'format' => 'dd/MM/yyyy',
+                'required' => false,
+                'widget' => 'single_text'
+            ])
             ->add('photo', FileType::class, [
                 'mapped' => false,
                 'required' => false,
-                'label' => 'Modifier la photo de profil du membre',
+                'label' => 'Modifier la photo de profil du membre'
             ])
-            ->add('formatPaiement', ChoiceType::class,
-                ['choices' => ['aucun' => 'aucun', 'cheque' => 'Chèque', 'especes' => 'Espèces', 'lydia' => Lydia], 'required' => true])
-            ->add('filiere', EntityType::class,
-                ['label' => 'Filiere',
-                 'class' => Filiere::class,
-                 'required' => true,
-                ])
-            ->add('securiteSociale', TextType::class,
-                ['required' => false, 'label' => 'Numéro de sécurité sociale'])
-            ->add('commentaire', TextareaType::class,
-                ['required' => false, 'label' => 'Commentaire libre']);
+            ->add('formatPaiement', ChoiceType::class, [
+                'choices' => [
+                    'aucun' => 'aucun',
+                    'cheque' => 'Chèque',
+                    'especes' => 'Espèces',
+                    'lydia' => 'Lydia'
+                ],
+                'required' => true
+            ])
+            ->add('filiere', EntityType::class, [
+                'label' => 'Filiere',
+                'class' => Filiere::class,
+                'required' => true
+            ])
+            ->add('securiteSociale', TextType::class, [
+                'required' => false,
+                'label' => 'Numéro de sécurité sociale'
+            ])
+            ->add('commentaire', TextareaType::class, [
+                'required' => false,
+                'label' => 'Commentaire libre'
+            ]);
     }
 
     public function getBlockPrefix()
