8d9e7c29be817221be79440fcebf9c62efe93f53
Loïc TRUPIN <loic.trupin@enseirb-mtmeca.fr>
2021-03-04 15:11:17 +0100

    install correction

--- install/only_install.sh
+++ install/only_install.sh
@@ -27,7 +27,7 @@ docker-compose up -d
 # Give time to boot DB container then load database schema & fixtures
 echo "Waiting some seconds before setting up the database"
 sleep 60
-#docker-compose exec web composer install
+docker-compose exec web composer install
 docker-compose exec web composer install:first
 echo "Installation is now complete. You can now log in with credentials admin/admin. Don't forget to change that password."
 
